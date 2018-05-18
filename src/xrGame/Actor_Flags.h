9629d8facc7c67cb7b5549e226e8a6c069ab71d7
v2v3v4 <terran.erre@mail.ru>
2018-05-18 20:40:28 +0300

    * ADD: Test commit

--- src/xrGame/Actor_Flags.h
+++ src/xrGame/Actor_Flags.h
@@ -2,17 +2,20 @@
 
 enum
 {
-    AF_GODMODE = (1 << 0),
-    AF_NO_CLIP = (1 << 1),
-    AF_UNLIMITEDAMMO = (1 << 3),
-    AF_RUN_BACKWARD = (1 << 4),
-    AF_AUTOPICKUP = (1 << 5),
-    AF_PSP = (1 << 6),
-    AF_DYNAMIC_MUSIC = (1 << 7),
-    AF_GODMODE_RT = (1 << 8),
-    AF_IMPORTANT_SAVE = (1 << 9),
-    AF_CROUCH_TOGGLE = (1 << 10),
-    AF_USE_TRACERS = (1 << 11),
+    AF_GODMODE          = (1 << 0),
+    AF_NO_CLIP          = (1 << 1),
+    AF_UNLIMITEDAMMO    = (1 << 3),
+    AF_RUN_BACKWARD     = (1 << 4),
+    AF_AUTOPICKUP       = (1 << 5),
+    AF_PSP              = (1 << 6),
+    AF_DYNAMIC_MUSIC    = (1 << 7),
+    AF_GODMODE_RT       = (1 << 8),
+    AF_IMPORTANT_SAVE   = (1 << 9),
+    AF_CROUCH_TOGGLE    = (1 << 10),
+    AF_USE_TRACERS      = (1 << 11),
+    AF_ZOOM_NEW_FD      =(1<<12),
+    AF_CAR_INFO         =(1<<13),
+    AF_HARDCORE         =(1<<14),
 };
 
 extern Flags32 psActorFlags;
